f5c2349cb626c29061a4f9419015c1adfbe2774a
szhou1 <zhousteve3@gmail.com>
2016-09-28 16:43:10 -0700

    Passing tests for SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,26 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
+  initialize: function(params) {
+
+    this.on('add', function(song) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', this.shift);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
